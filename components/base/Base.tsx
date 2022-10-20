62d1ab539c282a3ee978fe23f923829020e51edd
Ludwig SILVAIN <ludwig@silvain.eu>
2022-10-20 23:54:42 +0200

    feat : entêtes et favicon

--- components/base/Base.tsx
+++ components/base/Base.tsx
@@ -2,6 +2,7 @@ import BaseHeader from "./BaseHeader";
 import BaseFooter from "./BaseFooter";
 import styles from './Base.module.scss'
 import React from "react";
+import Head from "next/head";
 
 interface BaseProps {
     children?: JSX.Element
@@ -9,11 +10,36 @@ interface BaseProps {
 }
 
 const Base: React.FC<BaseProps> = ({children, mainTitle = false}) => {
-    return <div className={styles.content}>
-        <BaseHeader mainTitle={mainTitle}/>
-        <main className={styles.main}>{children}</main>
-        <BaseFooter/>
-    </div>
+    return <>
+
+        <Head>
+            <title>Ludwig SILVAIN</title>
+            <meta name="title" content="Ludwig SILVAIN"/>
+            <meta name="description" content="Curriculum Vitae de Ludwig SILVAIN (Développeur informatique)"/>
+            <link rel="icon" href="/favicon.ico"/>
+            <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://silvain.eu/"/>
+            <meta property="og:title" content="Ludwig SILVAIN"/>
+            <meta property="og:description" content="Ludwig SILVAIN (Développeur informatique)"/>
+            {/*<meta property="og:image" content="https://silvain.eu/img/site-screen.png"/>*/}
+
+
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://silvain.eu/"/>
+            <meta property="twitter:title" content="Ludwig SILVAIN"/>
+            <meta property="twitter:description" content="Ludwig SILVAIN (Développeur informatique)"/>
+            {/*<meta property="twitter:image" content="https://silvain.eu/img/site-screen.png"/>*/}
+
+        </Head>
+
+        <div className={styles.content}>
+            <BaseHeader mainTitle={mainTitle}/>
+            <main className={styles.main}>{children}</main>
+            <BaseFooter/>
+        </div>
+    </>
 }
 
 export default Base
--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -52,78 +52,73 @@ const Post: NextPage<PostProps> = ({project, images, technologiesImages}) => {
         return technologiesImages.find(x => x.toLowerCase().split(".")[0] == technology.replace(" ", "-").toLowerCase())
     }
 
-    return <>
-        <Head>
-            <title>Create Next App</title>
-        </Head>
+    return <Base>
+        <>
+            <div className={styles.projectArticle}>
+                <div className={styles.projectArticle__content}>
+                    <h1>{project.title}</h1>
 
-        <Base>
-            <>
-                <div className={styles.projectArticle}>
-                    <div className={styles.projectArticle__content}>
-                        <h1>{project.title}</h1>
+                    <Image alt={project.slug} src={require("./../../public/projects/" + project.image)}
+                           className={styles.art_miniature}/>
 
-                        <Image alt={project.slug} src={require("./../../public/projects/" + project.image)}
-                               className={styles.art_miniature}/>
+                    <div dangerouslySetInnerHTML={{__html: project.content}}></div>
 
-                        <div dangerouslySetInnerHTML={{__html: project.content}}></div>
-
-                        <div className={styles.projectArticle__gallery}>
-                            {images.map((x, index) =>
-                                <Link key={index} href={x}>
-                                    <a className={styles.projectArticle__gallery__item} target="_blank">
-                                        <Image src={require("./../../public/projects/" + x)} alt={x}/>
-                                    </a>
-                                </Link>
-                            )}
-                        </div>
-                    </div>
-
-                    <div className={styles.projectArticle__informations}>
-
-                        <div className={styles.projectArticle__informations__item}>
-                            <h2>Informations</h2>
-
-
-                            <div>
-                                <h4>Date de mise en production</h4>
-                                <p>{project.date}</p>
-                            </div>
-                            <div>
-                                <h4>Temps</h4>
-                                <p>{project.time}</p>
-                            </div>
-
-                            <div>
-                                <h4>Lien</h4>
-                                <p><a href={project.link} rel="noreferrer" target="_blank">{project.link}</a></p>
-                            </div>
-
-
-                        </div>
-                        <div>
-
-                            <h3>Technologies Utilisées</h3>
-                            <ul>
-                                {project.technologies.map((x, index) => {
-
-                                    const img = getTechnologyImage(x)
-                                    console.log(img)
-                                    return <li key={index}>
-                                        {img && <Image src={require("./../../public/technologies/" + img)} alt={x}
-                                                       width={24} height={24}/>}
-                                        <span>{x}</span>
-                                    </li>
-                                })}
-                            </ul>
-
-                        </div>
+                    <div className={styles.projectArticle__gallery}>
+                        {images.map((x, index) =>
+                            <Link key={index} href={x}>
+                                <a className={styles.projectArticle__gallery__item} target="_blank">
+                                    <Image src={require("./../../public/projects/" + x)} alt={x}/>
+                                </a>
+                            </Link>
+                        )}
                     </div>
                 </div>
-            </>
-        </Base>
 
-    </>
+                <div className={styles.projectArticle__informations}>
+
+                    <div className={styles.projectArticle__informations__item}>
+                        <h2>Informations</h2>
+
+
+                        <div>
+                            <h4>Date de mise en production</h4>
+                            <p>{project.date}</p>
+                        </div>
+                        <div>
+                            <h4>Temps</h4>
+                            <p>{project.time}</p>
+                        </div>
+
+                        <div>
+                            <h4>Lien</h4>
+                            <p><a href={project.link} rel="noreferrer" target="_blank">{project.link}</a></p>
+                        </div>
+
+
+                    </div>
+                    <div>
+
+                        <h3>Technologies Utilisées</h3>
+                        <ul>
+                            {project.technologies.map((x, index) => {
+
+                                const img = getTechnologyImage(x)
+                                console.log(img)
+                                return <li key={index}>
+                                    {img && <Image src={require("./../../public/technologies/" + img)} alt={x}
+                                                   width={24} height={24}/>}
+                                    <span>{x}</span>
+                                </li>
+                            })}
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    </Base>
+
+
 }
 
 export default Post;
