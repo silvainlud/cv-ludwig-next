5b3df8179cffeacad9f48d3488b646526e68b87e
Ludwig SILVAIN <ludwig@silvain.eu>
2022-10-30 18:08:33 +0100

    fix : lien sur les medias

--- components/base/Items/MediaList.tsx
+++ components/base/Items/MediaList.tsx
@@ -1,6 +1,5 @@
 import styles from './MediaList.module.scss'
 import Image from "next/future/image";
-import TwitterLogo from "../../../public/media/Logo black 1.svg";
 import GithubLogo from "../../../public/media/Octicons-mark-github 1.svg";
 import GitlabLogo from "../../../public/media/gitlab-logo-600 1.svg";
 import LinkedinLogo from "../../../public/media/LinkedIn_Logo 1.svg";
@@ -10,16 +9,14 @@ const MediaList = ({...otherProps}: HTMLProps<HTMLDivElement>) => {
     if (otherProps.className != undefined)
         otherProps.className += " " + styles.media;
     return <div className={styles.media} {...otherProps}>
-        <a className={styles.media__item} href="">
-            <Image src={TwitterLogo} alt="Twitter"/>
-        </a>
-        <a className={styles.media__item} href="">
+        <a className={styles.media__item} href="https://github.com/silvainlud" target="_blank" rel="noreferrer">
             <Image src={GithubLogo} alt="Github"/>
         </a>
-        <a className={styles.media__item} href="">
+        <a className={styles.media__item} href="https://gitlab.com/silvainlud" target="_blank" rel="noreferrer">
             <Image src={GitlabLogo} alt="Gitlab"/>
         </a>
-        <a className={styles.media__item} href="">
+        <a className={styles.media__item} href="https://www.linkedin.com/in/ludwig-silvain/" target="_blank"
+           rel="noreferrer">
             <Image src={LinkedinLogo} alt="Linkedin"/>
         </a>
     </div>
